c76fdfe8608d0eabb7cc7673f33847ad345528bb
marta <marta.ben.col@gmail.com>
2025-11-03 21:33:41 +0100

    [FEATURE] Backend

---
+++ index.js
@@ -0,0 +1,94 @@
+const morgan = require('morgan')
+const express = require('express')
+const app = express()
+
+//midlewares
+morgan.token('request-data', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status - :response-time ms :request-data'))
+app.use(express.json())
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+      response.json(person)
+  } else {
+      response.statusMessage = "Person not found";
+      response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing' 
+    })
+  }
+
+  if(persons.findIndex(person => person.name.toLowerCase() === body.name.toLowerCase()) != -1){
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 99999999999),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+  `)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
+]
