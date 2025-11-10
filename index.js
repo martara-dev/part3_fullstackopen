7d154503e0ccf90f89ba50b673e49b720f7c12c5
marta <marta.ben.col@gmail.com>
2025-11-10 11:43:04 +0100

    [FEATURE] Added mongodb

--- index.js
+++ index.js
@@ -1,97 +1,94 @@
+require('dotenv').config() 
 const morgan = require('morgan')
 const express = require('express')
 const cors = require('cors')
+const Person = require('./models/person') 
 const app = express()
 
-//midlewares
+//cors
 app.use(cors())
+
+//morgan
 morgan.token('request-data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status - :response-time ms :request-data'))
+
+//express
 app.use(express.json())
-app.use(express.static('dist'))
 
+//endpoints
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-      response.json(person)
-  } else {
-      response.statusMessage = "Person not found";
-      response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end()
+  }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name missing' 
-    })
-  }
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number missing' 
-    })
-  }
-
-  if(persons.findIndex(person => person.name.toLowerCase() === body.name.toLowerCase()) != -1){
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 99999999999),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then(personSaved => {
+      response.json(personSaved)
+  }).catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(request.params.id, 
+    { name, number },
+    { new: true, runValidators: true, context: 'query' })
+    .then(personUpdated => {
+      response.json(personUpdated)
+    }).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  response.send(`
-      <p>Phonebook has info for ${persons.length} people</p>
+  Person.find({}).then(people => {
+    response.send(`
+      <p>Phonebook has info for ${people.length} people</p>
       <p>${new Date()}</p>
-  `)
+    `)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+//unknownEndpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+//errorHandler
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+//run server
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
